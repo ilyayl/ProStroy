a9660ec47eb3eea3b5dd3fa3e46be6b548fbf942
ilyayl <pomortsev.il@gmail.com>
2025-09-23 22:51:14 +0300

    Update header.js

--- header.js
+++ header.js
@@ -5,7 +5,9 @@ const PROJECT_URL = "https://hvpbwpegxcbstmpngdyc.supabase.co";
 const ANON_KEY    = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2cGJ3cGVneGNic3RtcG5nZHljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2NDE4NjMsImV4cCI6MjA3NDIxNzg2M30.rtPrQVsaEFA-ee1RphLKKn8q3TSOXeapZnZgfe9HVws";
 
 // SDK Supabase уже загружен на странице, создаём клиент
-window.supa = window.supabase.createClient(PROJECT_URL, ANON_KEY);
+window.supa = window.supabase.createClient(PROJECT_URL, ANON_KEY, {
+  auth: { persistSession: true, storage: window.sessionStorage }
+});
 
 /** Подтягивает header.html и настраивает меню/имя пользователя */
 async function loadHeader(targetId = "site-header") {
